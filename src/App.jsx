c3123705344a7bfaff286b9a0a635f01a5e5dea2
AlanContreras784 <alancontreras784@gmail.com>
2025-05-16 22:20:13 -0300

    actualizando App2

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ function App() {
       <div>
         <Header productosCarrito={productosCarrito}/>
         <Routes>
-          <Route path="/" element={<Home/>} />
+          <Route path="pre-entrega-TT-React-js/" element={<Home/>} />
           <Route path="/about" element={<About/>}/>
           <Route path="/contacto" element={<FormularioConSweetAlert/>}/>
           <Route path="/productos" element={<ProductosContainer functionCarrito={funcionCarrito}/>} />
