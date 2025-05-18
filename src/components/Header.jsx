da47af5da96540f5922418dc9cae108413574f99
AlanContreras784 <alancontreras784@gmail.com>
2025-05-17 23:21:40 -0300

    cambiar direccion y carrito denton de Nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,13 @@ import "../styles/Header.css"
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from "react-router-dom";
 
 function Header({productosCarrito}) {
     return (
         <header className="header ">
             <Navbar variant="dark" expand="lg" className="fs-5 py-0">
-                <Container fluid >
+                
                     <Navbar.Brand href="#home"><img className="logo  me-auto" src={Logo} alt="" />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav " className="ms-auto"/>
@@ -21,11 +20,12 @@ function Header({productosCarrito}) {
                             <Link className="me-auto nav-link" to="/about">Nosotros</Link>
                             <Link className="me-auto nav-link" to="/productos">Productos</Link>
                             <Link className="me-auto nav-link" to="/contacto">Contacto</Link>
+                            <Link className=" iconCarrito mx-0 px-4 me-auto  nav-link" to="/carrito"><i class="fa-solid fa-cart-shopping"></i><small>{productosCarrito.length>0 ? productosCarrito.length : ""}</small></Link>
                         </Nav>
                     </Navbar.Collapse>
-                    <Link className=" iconCarrito mx-0 px-4  nav-link" to="/carrito"><i class="fa-solid fa-cart-shopping"></i><span>{productosCarrito.length>0 ? productosCarrito.length : ""}</span></Link>
                     
-                </Container>
+                    
+                
             </Navbar>
         </header>
     
