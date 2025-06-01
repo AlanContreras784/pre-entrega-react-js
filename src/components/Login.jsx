89e59154e571223f885d84fc1535982e45cf5a19
AlanContreras784 <alancontreras784@gmail.com>
2025-06-01 00:47:50 -0300

    login con firebase. uso del useContext en authContext, y estilizando algunas paginas

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,4 @@
-/* import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function Login({setLogueadoUser, setLogueadoAdmi, user, admi}){
-    return(
-        <div>
-            <button className="btn btn-outline-primary mx-3 my-5 fs-5"  onClick={setLogueadoUser}>{user? "Cerrar Sesión" : "Iniciar Sesión"}</button>
-            <button className="btn btn-outline-warning mx-3 my-5 fs-5" onClick={setLogueadoAdmi}>{admi? "Cerrar como Admin" : "Iniciar como Admin"}</button>
-        </div>
-    )
-} */
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
